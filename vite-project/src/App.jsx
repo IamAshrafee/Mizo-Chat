b426e4259e274f160f053e924551e893ed97dbf4
Abdullah Al Ashrafee <dev.ashrafee@gmail.com>
2025-07-01 04:06:45 +0600

    Registration page design done and some validations also implemented

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Registration from "./Components/Pages/Registration";
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/registration",
+    element: <Registration />,
+  },
+]);
 
 const App = () => {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
